a25c87482b25fa71826277b3dda991c6575e8859
Kaouther TRABELSI <kaoutharou98@gmail.com>
2024-06-30 13:46:07 +0200

    réaliser pages accueil et galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,7 @@ import Route from "./Route.js"
 
 export const  allRoutes= [
     new Route("/", "Accueil", "pages/home.html"),
-    new Route("/Galerie", "Galerie", "pages/galerie.html"),
+    new Route("/galerie", "Galerie", "pages/galerie.html"),
 ]
 
 export const websiteName = "Quai Antique";
